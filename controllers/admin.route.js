fcf3e18a8c3755a0df1451bf13c1c94f48bc0b4e
daoleviethoang <daoleviethoang@github.com>
2021-01-15 07:02:08 +0700

    create admin

---
+++ controllers/admin.route.js
@@ -0,0 +1,10 @@
+// TODO: check invalid username
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const moment = require('moment');
+const userModel = require('../models/user.model');
+const studentModel = require('../models/student.model');
+const teacherModel = require('../models/teacher.model');
+const auth = require('../middlewares/auth.mdw');
+const router = express.Router();
+
